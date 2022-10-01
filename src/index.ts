a1ad7b7a23d5dc40fdef5a0127ed757f0d66cd88
sharglut <p.filimon75@gmail.com>
2022-10-01 16:45:47 +0200

    preparation compiler ts

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+/* Tu vas devoir mettre en place une fonction qui renvoie une liste de prénoms
+contenant la sous-chaine de caractère passée en paramètre  
+*/
+
+const names: string[] = [
+  "Paul",
+  "Ludovic",
+  "Etienne",
+  "Johann",
+  "Jeremy",
+  "Louis",
+  "Floriane",
+  "Florian",
+  "Nicolas",
+  "Mouss",
+  "Safae",
+  "Mathieu",
+  "Eric",
+  "Michael",
+  "Theo",
+  "Ronee",
+  "Soeli",
+  "Philippe",
+  "Josselin",
+];
+
+function Search(search: string, listName: string[]): string[] {}
